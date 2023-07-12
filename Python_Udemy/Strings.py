f2a868e2c9e8f199379ce0561557cf6e2cbca828
aiman.wahizam <aiman.wahizam@nology.io>
2023-07-12 20:04:06 +0100

    added slicing and indexing notes

--- Python_Udemy/Strings.py
+++ Python_Udemy/Strings.py
@@ -1,10 +1,14 @@
 string = "hello"
+
+#####Slicing and indexing
 first_letter = string[0]
+
+#This shows that you can use negative index to go from the back
 all_but_last_letter = string[:-1]
 print(first_letter)
 print(all_but_last_letter)
 
-#.format()
+#####.format()
 print("The {} {} {}".format("fox", "brown", "quick"))
 
 #you can also make it .format ref the in the correct order
