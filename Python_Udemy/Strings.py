7e17ad681171a237cc807dd5a0979948672e9420
aiman.wahizam <aiman.wahizam@nology.io>
2023-07-12 20:23:39 +0100

    fstrings

--- Python_Udemy/Strings.py
+++ Python_Udemy/Strings.py
@@ -13,3 +13,25 @@ print("The {} {} {}".format("fox", "brown", "quick"))
 
 #you can also make it .format ref the in the correct order
 print("The {2} {1} {0}".format("fox", "brown", "quick"))
+
+#you can also use variables for .format
+print("The {q} {b} {f}".format(f="fox", b="brown", q="quick"))
+
+#you can also do this with variables
+result = 10/3
+print("10/3 = {result}".format(result=result))
+#or
+print("10/3 = {}".format(result))
+
+#you can also edit the number of decimals places used in .format => {value called:width.precision f}
+#width is how wide the result will be (normally 1)
+#precision is decimal places (rounded up if needs be)
+print("10/3 to the nearest 2 decimal places is {result:1.2f}".format(result=result))
+
+#####fstrings
+#same as above but different way
+name = "Aiman"
+age = 24
+#Can write print("{name} is {age} years old".format(name=name, age=age))
+#or print("{} is {} years old".format(name, age))
+print(f"{name} is {age} years old")
