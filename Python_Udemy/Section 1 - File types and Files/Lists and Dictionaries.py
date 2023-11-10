a9631f92d39fbe9d0e2df0dbc1fd880cb9bd6052
aiman.wahizam <aiman.wahizam@nology.io>
2023-11-10 13:45:40 +0400

    Reviewed Lists and Dict notes and made clear

--- Python_Udemy/Section 1 - File types and Files/Lists and Dictionaries.py
+++ Python_Udemy/Section 1 - File types and Files/Lists and Dictionaries.py
@@ -4,6 +4,10 @@ my_list = [1,2,3]
 #                                 Dictionaries                                 #
 # ---------------------------------------------------------------------------- #
 
+# ---------------------------------------------------------------------------- #
+#                          Accessing Dictionary Values                         #
+# ---------------------------------------------------------------------------- #
+
 my_dict = {
     "name":"Aiman",
     "age":24
@@ -11,40 +15,68 @@ my_dict = {
 
 print(my_dict["name"])
 
+# ---------------------------------------------------------------------------- #
+#                      Accessing Dictionary Values Part 2                      #
+# ---------------------------------------------------------------------------- #
+
 # Can hold lists and dictionaries inside dictionaries
 my_dict_two = {
     "name": "Aiman",
-    "lived places": ["Malaysia", "Qatar", "UK"],
-    "siblings": {
-        "Aisyah": 21,
-        "Akmal": 13
-    }
+    "lived places": ["Malaysia", "Qatar", "UK", "UAE"],
+    "siblings": [
+        {
+            "name": "Aisyah",
+            "age": 21,
+            "relation": "Sister"
+        },
+        {
+            "name": "Akmal",
+            "age": 13,
+            "relation": "Brother"
+        }
+    ]
 }
 
+# NOTE: f statements are used to print WITH VALUES in more understandable way
 print(f"Places I have lived: {my_dict_two['lived places']}")
 
-#Note calling a value of a dict inside a dict is next to and not in 
-print(f"Age of my sister is {my_dict_two['siblings']['Aisyah']}")
+# NOTE: calling a value of a dict inside a dict is NEXT TO and not in (nested)
+print(f'Age of my sister is {my_dict_two["siblings"][0]["age"]}')
 
-#Note calling a value of a list inside a dict is next to and not in 
+# More advanced example
+print(f"Age of my {my_dict_two['siblings'][0]['relation'].lower()} is {my_dict_two['siblings'][0]['age']}")
+
+# NOTE: calling a value of a list inside a dict is next to and not in 
 print(f"Last placed I have lived is {my_dict_two['lived places'][-1]}")
 
-#adding new key value pair
-my_dict_two["hobbies"] = ["gaming", "climbing"]
-print(my_dict_two)
+# ---------------------------------------------------------------------------- #
+#                      Accessing Dictionary Values Part 3                      #
+# ---------------------------------------------------------------------------- #
 
-#overwrite key value pair
-my_dict_two["name"] = "Aiman Wahizam"
-print(my_dict_two)
-
-#to get all keys
+# To get all keys
 keys = my_dict_two.keys()
 print(keys)
 
-#to get all values
+# To get all values
 values = my_dict_two.values()
 print(values)
 
-#to get all pairs
+# To get all pairs
 items = my_dict_two.items()
 print(items)
+
+# ---------------------------------------------------------------------------- #
+#                            Adding to a Dictionary                            #
+# ---------------------------------------------------------------------------- #
+
+# Adding new key value pair
+my_dict_two["hobbies"] = ["gaming", "climbing"]
+print(my_dict_two)
+
+# Overwrite key value pair
+my_dict_two["name"] = "Aiman Wahizam"
+print(my_dict_two)
+
+# Advanced example
+my_dict_two["siblings"][0]["name"] = "Aisyah Wahizam"
+print(my_dict_two)
