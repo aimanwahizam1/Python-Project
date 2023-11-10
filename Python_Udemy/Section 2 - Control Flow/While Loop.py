411cb3a44dcfd27ac4ed9d99103fec4045036ae9
aiman.wahizam <aiman.wahizam@nology.io>
2023-11-10 18:58:06 +0400

    Reviewed While Loops and made more clear

--- Python_Udemy/Section 2 - Control Flow/While Loop.py
+++ Python_Udemy/Section 2 - Control Flow/While Loop.py
@@ -4,21 +4,21 @@
 
 x = [1,2,3]
 
-for item in x:
-    #can use Pass as filler so no syntax error when coding
-    #pass does nothing at all
-    pass
+""" for item in x:
+    # Can use Pass as filler so no syntax error when coding
+    # Pass does nothing at all
+    pass """
 
 my_string = 'Aiman'
 
-for letter in my_string:
+""" for letter in my_string:
     if letter.lower() == 'a':
-        #Use continue to go back to top of while loop
+        # Use continue to go back to top of while loop
         continue
-    print(letter)
+    print(letter) """
 
-for letter in my_string:
+""" for letter in my_string:
     if letter.lower() == 'm':
-        #Use break to go get out of while loop
+        # Use break to go get out of while loop
         break
-    print(letter)
+    print(letter) """
