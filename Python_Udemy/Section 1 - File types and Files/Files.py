9bca08fefaf51bf85547f0563507a1dabb860546
aiman.wahizam <aiman.wahizam@nology.io>
2023-11-10 13:03:25 +0400

    Reviewed and edited files.py for clarity

--- Python_Udemy/Section 1 - File types and Files/Files.py
+++ Python_Udemy/Section 1 - File types and Files/Files.py
@@ -2,57 +2,79 @@
 #                                     Files                                    #
 # ---------------------------------------------------------------------------- #
 
-#Working with test.txt
+# --------------------------- Working with test.txt -------------------------- #
+
 #print(os.getcwd())
 #Statement below means we do not need to .close() at end of code when we first .open()
-""" with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File typs and Files/test.txt') as myfile:
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt') as myfile:
     contents = myfile.read()
-    print(myfile.read())
+    print('Reading for the first time:')
+    print(contents + '\n')
 
     #to read again you need to reset cursor
     myfile.seek(0)
-    print('Reading for the second time:\n' + myfile.read())
+    print('Reading for the second time:\n' + contents)
+    print('\n')
 
-    #print as seperate lines
+    #print as separate lines
     myfile.seek(0)
     print('Separate lines:\n', myfile.readlines())
+    print('\n')
 
-#Variables are global
+# #Variables are global
+print('Global Reading:')
 print(contents) """
 
-# ------------------------- Files reading and writing ------------------------ #
+# ---------------------------------------------------------------------------- #
+#                           Files reading and writing                          #
+# ---------------------------------------------------------------------------- #
 
-#Reading
-""" with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/test.txt', mode='r') as myfile_read:
+# ---------------------------------- Reading --------------------------------- #
+
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt', mode='r') as myfile_read:
     contents_read = myfile_read.read()
     print(contents_read) """
 
-#Writing
-""" with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/test.txt', mode='w') as myfile_read:
+# ---------------------------------- Writing --------------------------------- #
+
+# NOTE: any writing will overwrite any existing text in the current file - Don't run this (illustration purpose only)
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt', mode='w') as myfile_read:
+    contents_read = myfile_read.write('test write') """
+
+# Fail Writing Case (also applies to Append)
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt', mode='r') as myfile_read:
     #.read will fail since we are in write mode (vice versa)
     contents_read = myfile_read.read()
     print(contents_read) """
 
-#Append
-""" with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/test.txt', mode='a') as myfile_read:
+# ---------------------------------- Append ---------------------------------- #
+
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt', mode='a') as myfile_append:
     #.read will fail since we are in write mode (vice versa)
-    contents_read = myfile_read.read()
-    print(contents_read) """
+    contents_append = myfile_append.write('\n' + 'test append') """
 
-# ---------------------------------- Part 2 ---------------------------------- #
+# Read the append to see it in action
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/test.txt') as myfile:
+    print(myfile.read()) """
 
-""" with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/my_new_test.txt', mode='r') as my_new_file:
+
+# ---------------------------------------------------------------------------- #
+#                       Files Part 2 - Methods in Action                       #
+# ---------------------------------------------------------------------------- #
+
+# 1. Read File (Original will only have 3 lines)
+""" with open('C:/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/Text files/my_new_test.txt', mode='r') as my_new_file:
     print(my_new_file.read()) """
 
-#Append new line
+# 2. Append new line
 """ with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/my_new_test.txt', mode='a') as my_new_file:
     my_new_file.write('\nFOUR ON FOURTH') """
 
-#Read will have new line 
+# 3. Read will have new line 
 """ with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/my_new_test.txt', mode='r') as my_new_file:
     print(my_new_file.read()) """
 
-#If file didnt exist, this would make a new one with mode 'w'
+# NOTE: If file doesn't exist, this would make a new one with mode 'w'
 """ with open('/c/Users/aiman/Python Project/Python_Udemy/Section 1 - File types and Files/my_new_test_write.txt', mode='w') as my_new_file_write:
     my_new_file_write.write('I created this file!') """
 
--- Python_Udemy/Section 3 - Methods and Functions/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    no_space = backwards.replace(" ","")
-    print(no_space)
