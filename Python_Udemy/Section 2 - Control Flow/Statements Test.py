38b48eb9bda6dce9dc38ffbd1ae5f457f7f242cf
aiman.wahizam <aiman.wahizam@nology.io>
2023-11-10 15:42:08 +0400

    Redid test and made answers clearer

--- Python_Udemy/Section 2 - Control Flow/Statements Test.py
+++ Python_Udemy/Section 2 - Control Flow/Statements Test.py
@@ -1,29 +1,62 @@
+# ---------------------------------------------------------------------------- #
+#                                  Question 1                                  #
+# ---------------------------------------------------------------------------- #
+
 #Use for, .split(), and if to create a Statement that will print out words that start with 's':
 st = 'Print only the words that start with s in this sentence'
-s_words = [word for word in st.split() if word[0].lower() == 's']
-#print(s_words)
+
+# --------------------------------- Solutions -------------------------------- #
+
+# 1.
+""" s_words = [word for word in st.split() if word[0].lower() == 's'] """
+# 2. 
+""" s_words = [word.lower() for word in st.split() if word.lower().startswith('s')]
+
+print(s_words) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 2                                  #
+# ---------------------------------------------------------------------------- #
 
 #Use range() to print all the even numbers from 0 to 10.
-even_numbers = [number for number in range(0,11,2)]
-#print(even_numbers)
+
+# --------------------------------- Solution --------------------------------- #
+
+""" even_numbers = [number for number in range(0,11,2)]
+print(even_numbers) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 3                                  #
+# ---------------------------------------------------------------------------- #
 
 #Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
-numbers_divisible_3 = [number for number in range(0,50) if number%3==0]
-#print(numbers_divisible_3)
 
-#Go through the string below and if the length of a word is even print "even!"
+# --------------------------------- Solution --------------------------------- #
+
+""" numbers_divisible_3 = [number for number in range(0,50) if number % 3 == 0]
+print(numbers_divisible_3) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 4                                  #
+# ---------------------------------------------------------------------------- #
+
+#Go through the string below and and give a list of even length words"
 st = 'Print every word in this sentence that has an even number of letters'
-even_words = []
 
-for word in st.split():
-    if len(word) % 2 == 0:
-        even_words.append("even!")
-        continue
-    even_words.append(word)
+# --------------------------------- Solution --------------------------------- #
 
-#print(even_words)
+""" even_words = [word for word in st.split() if len(word) % 2 == 0]
+print(even_words) """
 
-#Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+# ---------------------------------------------------------------------------- #
+#                                  Question 5                                  #
+# ---------------------------------------------------------------------------- #
+
+# Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz".
+# For numbers which are multiples of both three and five print "FizzBuzz".
+
+# --------------------------------- Solution --------------------------------- #
+
 numbers_list = [number for number in range(0,101)]
 numbers_list_output =[]
 
@@ -38,10 +71,15 @@ for number in numbers_list:
     else:
         numbers_list_output.append(number)
 
-#print(numbers_list_output)
+print(numbers_list_output)
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 6                                  #
+# ---------------------------------------------------------------------------- #
 
 #Use List Comprehension to create a list of the first letters of every word in the string below:
 st = 'Create a list of the first letters of every word in this string'
-first_letters = [word[0] for word in st.split()]
 
-print(first_letters)
+# --------------------------------- Solution --------------------------------- #
+""" first_letters = [word[0] for word in st.split()]
+print(first_letters) """
