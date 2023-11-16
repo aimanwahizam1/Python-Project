4125c7bc90f2be5672fab916a5e9e3567dd848db
aiman.wahizam <aiman.wahizam@nology.io>
2023-11-16 13:54:58 +0400

    Redid functions test

--- Python_Udemy/Section 3 - Methods and Functions/Functions Test.py
+++ Python_Udemy/Section 3 - Methods and Functions/Functions Test.py
@@ -1,30 +1,35 @@
+# ---------------------------------------------------------------------------- #
+#                                  Question 1                                  #
+# ---------------------------------------------------------------------------- #
+
 # Write a function that computes the volume of a sphere given its radius.
+
 import math
 
-def vol(rad):
-    return (4/3)*math.pi*(rad**3)
+""" def vol(rad):
+    return (4/3)*math.pi*(rad**3) """
 
-#print(vol(2))
+""" print(vol(2)) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 2                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a function that checks whether a number is in a given range (inclusive of high and low)
 
-#Returns statement version
-def ran_check_1(num,low,high):
-    if num >= low and num <= high:
-        return f'{num} is in the range between {low} and {high}'
+""" def ran_check_1(num,low,high):
+    return num >= low and num <= high """
     
-#print(ran_check_1(5,2,7))
-#print(ran_check_1(3,1,10))
+""" print(ran_check_1(5,2,7))
+print(ran_check_1(3,1,10)) """
 
-#Returns boolean version
-def ran_check_2(num,low,high):
-    return num >= low and num <= high
-
-#print(ran_check_2(5,2,7))
-#print(ran_check_2(3,1,10))
+# ---------------------------------------------------------------------------- #
+#                                  Question 3                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
-def up_low(s):
+
+""" def up_low(s):
     counter_dictionary = {
         'upper': 0,
         'lower': 0
@@ -37,49 +42,63 @@ def up_low(s):
             else:
                 counter_dictionary['lower'] += 1
     
-    return f'Original String: {s} \nNo. of Upper case characters: {counter_dictionary["upper"]} \nNo. of Lower case characters: {counter_dictionary["lower"]} '
+    return f'Original String: {s} \nNo. of Upper case characters: {counter_dictionary["upper"]} \nNo. of Lower case characters: {counter_dictionary["lower"]} ' """
 
-#print(up_low('Hello Mr. Rogers, how are you this fine Tuesday?'))
+""" print(up_low('Hello Mr. Rogers, how are you this fine Tuesday?')) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 4                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a Python function that takes a list and returns a new list with unique elements of the first list.
-def unique_list(lst):
-    return list(set(lst))
 
-#print(unique_list([1,1,1,1,2,2,3,3,3,3,4,5]))
+""" def unique_list(lst):
+    return list(set(lst)) """
+
+""" print(unique_list([1,1,1,1,2,2,3,3,3,3,4,5])) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 5                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a Python function to multiply all the numbers in a list.
-def multiply(numbers):  
+
+""" def multiply(numbers):  
     output = 1
 
     for number in numbers:
         output *= number
 
-    return output
+    return output """
 
-#print(multiply([1, 2, 3, -4]))
+""" print(multiply([1, 2, 3, -4])) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 6                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a Python function that checks whether a word or phrase is palindrome or not.
-def palindrome(s):
-    return s.replace(" ","") == s[::-1].replace(" ","")
 
-#print(palindrome('nurses run'))
-#print(palindrome('hello'))
+""" def palindrome(s):
+    return s.replace(" ","") == s[::-1].replace(" ","") """
+
+""" print(palindrome('nurses run'))
+print(palindrome('hello')) """
+
+# ---------------------------------------------------------------------------- #
+#                                  Question 7                                  #
+# ---------------------------------------------------------------------------- #
 
 # Write a Python function to check whether a string is pangram or not. (Assume the string passed in does not have any punctuation)
 import string
 
-def ispangram(str1, alphabet=string.ascii_lowercase):
-    letters = set(str1.lower().replace(" ",""))
+""" def ispangram(str1, alphabet=string.ascii_lowercase):
+    letters = [letter.lower() for letter in str1.replace(" ", "")]
 
-    if len(alphabet) == len(letters.intersection(alphabet)):
-        return True
+    return len(set(letters)) == len(alphabet) """
 
-#print(ispangram("The quick brown fox jumps over the lazy dog"))
-#print(ispangram("Pack my box with five dozen liquor jugs"))
-
-
-
-print(list(range(1,10,3)))
+""" print(ispangram("The quick brown fox jumps over the lazy dog"))
+print(ispangram("Pack my box with five dozen liquor jugs")) """
 
 
 
