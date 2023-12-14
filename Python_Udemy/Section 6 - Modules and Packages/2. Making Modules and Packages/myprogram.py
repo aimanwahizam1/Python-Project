c1b77ac5a059237efa5e0d3e4d17d12b740754ff
aiman.wahizam <aiman.wahizam@nology.io>
2023-12-14 14:36:24 +0400

    Modules and Packages lesson done

--- Python_Udemy/Section 6 - Modules and Packages/2. Making Modules and Packages/myprogram.py
+++ Python_Udemy/Section 6 - Modules and Packages/2. Making Modules and Packages/myprogram.py
@@ -11,6 +11,10 @@ my_func() """
 #                            Importing from Package                            #
 # ---------------------------------------------------------------------------- #
 
+# NOTE: To create a package you need to make a __init__.py
+# This tells python that this is a package
+# This is seen in MyMainPackage and MyMainPackage.SubPackage
+
 from MyMainPackage import some_main_script
 from MyMainPackage.SubPackage import my_sub_script
 
