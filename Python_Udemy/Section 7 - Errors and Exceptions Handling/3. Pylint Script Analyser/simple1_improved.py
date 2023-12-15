71bd023130ba796fd788bc41769240a129ea62c6
aiman.wahizam <aiman.wahizam@nology.io>
2023-12-15 12:32:27 +0400

    Pylint lesson done

---
+++ Python_Udemy/Section 7 - Errors and Exceptions Handling/3. Pylint Script Analyser/simple1_improved.py
@@ -0,0 +1,17 @@
+"""
+This is a module docstring. It should tell users of this module what it does.
+
+E.g. This module prints two variables a and b
+This module aims to use result of pylint report of simple1.py to increase score.
+"""
+
+def myfunc():
+    """
+    This is a function docstring. It should tell the users what this function does.
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
